4ffb9ef5785b551232f0ef9d442be5a767634871
avido <sebastiaan@avido.nl>
2018-07-17 08:27:17 +0200

    Enabled test mode for phpunit

--- tests/BolPlazaClientTest.php
+++ tests/BolPlazaClientTest.php
@@ -35,7 +35,7 @@ class BolPlazaClientTest extends TestCase
         $privateKey = getenv('PHP_PRIVKEY');
 
         $this->client = new BolPlazaClient($publicKey, $privateKey);
-        $this->client->setTestMode(false);
+        $this->client->setTestMode(true);
     }
 
     public function testOrderRetrieve()
@@ -529,7 +529,10 @@ class BolPlazaClientTest extends TestCase
     }
     
     /**
-     * @group inbound
+     * Get summarized LvB inbound-list
+     * @see https://developers.bol.com/inbound-list/
+     * Ignored, not present in sandbox
+     * @group no-ci-test
      */
     public function testGetInbounds()
     {
